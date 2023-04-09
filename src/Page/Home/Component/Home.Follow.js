dfffbd3554678289cc0fa5c681d56c10abe2f293
AppleCup12 <peun955@gmail.com>
2023-04-09 14:26:05 +0700

    update

--- src/Page/Home/Component/Home.Follow.js
+++ src/Page/Home/Component/Home.Follow.js
@@ -5,10 +5,10 @@ export default function Home_Follow() {
         <div className='relative flex justify-center w-full h-full'>
             <div className='w-[80%] relative pb-[5%]'>
                 <div className='text-center w-full'>
-                    <p className='font-bold text-[42px]'>Follow Facebook</p>
+                    <p className='font-bold lg:text-[42px] text-[22px]'>Follow Facebook</p>
                     <p className='text-[18px]'>@Apple Phomsungcan</p>
                 </div>
-                <div className='grid grid-cols-5 gap-5 py-10'>
+                <div className='grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-5 py-10'>
                     <div>
                         <a href='https://www.facebook.com/' target='_blank' className='w-[50vh] h-[50vh]'>
                             <img src='https://freebw.com/templates/novas/images/hp-1-ig-1.jpg' className='object-cover' />
